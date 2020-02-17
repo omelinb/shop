41267c2a31668013a4170b5a5e4465d05beb42b8
omelinb <elros.ob@gmail.com>
2020-02-17 16:23:55 +0300

    Changed action names; added stub to cart controller

--- app/controllers/api/carts_controller.rb
+++ app/controllers/api/carts_controller.rb
@@ -1,13 +1,23 @@
 class Api::CartsController < ApplicationController
+  # include CurrentCart
+  before_action :set_cart, only: %i[add_product show remove_product]
+
   # POST /api/cart
-  def add
+  def add_product
   end
 
   # GET /api/cart
   def show
+    render json: @cart
   end
 
   # DELETE /api/cart/{product_id}
-  def remove
+  def remove_product
   end
+
+  private
+
+    def set_cart
+      @cart = Cart.all.last
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     get    'products',         to: 'products#index'
-    post   'cart',             to: 'carts#add'
+    post   'cart',             to: 'carts#add_product'
     get    'cart',             to: 'carts#show'
-    delete 'cart/:product_id', to: 'carts#remove'
+    delete 'cart/:product_id', to: 'carts#remove_product'
   end
 end
