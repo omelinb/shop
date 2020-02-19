a22227a05e0153f145fb25171593a210750c5279
omelinb <elros.ob@gmail.com>
2020-02-20 01:19:48 +0300

    Added some tests

--- test/controllers/api/carts_controller_test.rb
+++ test/controllers/api/carts_controller_test.rb
@@ -1,19 +1,28 @@
 require 'test_helper'
 
 class Api::CartsControllerTest < ActionDispatch::IntegrationTest
-  test "should get add" do
-    get api_carts_add_url
+  test "should not add because of quantity missing" do
+    post api_cart_url, params: { product_id: 1 }
+    assert_response :bad_request
+  end
+
+  test "should not add because of product_id missing" do
+    post api_cart_url, params: { quantity: 1 }
+    assert_response :bad_request
+  end
+
+  test "should not add because of bad param" do
+    post api_cart_url, params: { product_id: 10, quantity: 10 }
+    assert_response :bad_request
+  end
+
+  test "should add" do
+    post api_cart_url, params: { product_id: 2, quantity: 5 }
     assert_response :success
   end
 
-  test "should get show" do
-    get api_carts_show_url
+  test "should show" do
+    get api_cart_url
     assert_response :success
   end
-
-  test "should get remove" do
-    get api_carts_remove_url
-    assert_response :success
-  end
-
 end
--- test/controllers/api/products_controller_test.rb
+++ test/controllers/api/products_controller_test.rb
@@ -2,8 +2,7 @@ require 'test_helper'
 
 class Api::ProductsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get api_products_index_url
+    get api_products_url
     assert_response :success
   end
-
 end
